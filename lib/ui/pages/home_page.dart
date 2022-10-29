ce61882ea3803535b24030bd0c47b674ba52bb76
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-29 10:16:15 -0500

    Dividers 2

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:codigo2_qr/ui/widgets/general_widget.dart';
 import 'package:codigo2_qr/utils/assets.dart';
 import 'package:flutter/material.dart';
@@ -14,26 +13,67 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
-              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6.0),
-              color: Colors.blue,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+              padding: const  EdgeInsets.symmetric(horizontal: 12, vertical: 14.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(14.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.06),
+                    blurRadius: 12,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
               child: Row(
                 children: [
                   Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Lorem ipsum dolor sit amet",),
-                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",),
+                        Text(
+                          "Lorem ipsum dolor sit amet",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        divider3,
+                        Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+                          style: TextStyle(
+                            height: 1.2,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        divider6,
                         Row(
                           children: [
-                            SvgPicture.asset(Assets.iconCalendar,),
-                            Text("12/12/2022",),
-
-                            SvgPicture.asset(Assets.iconTime,),
-                            Text("12/12/2022",),
+                            SvgPicture.asset(
+                              Assets.iconCalendar,
+                              height: 16.0,
+                              color: Colors.black54,
+                            ),
+                            Text(
+                              "12/12/2022",
+                              style: TextStyle(
+                                color: Colors.black54,
+                              ),
+                            ),
+                            dividerWidth6,
+                            SvgPicture.asset(
+                              Assets.iconTime,
+                              height: 16.0,
+                              color: Colors.black54,
+                            ),
+                            Text(
+                              "10:30 am",
+                              style: TextStyle(
+                                color: Colors.black54,
+                              ),
+                            ),
                           ],
                         ),
                       ],
@@ -43,7 +83,6 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-
           ],
         ),
       ),
