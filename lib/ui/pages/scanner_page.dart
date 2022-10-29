119b51cb16c791abe07b0da180b81cf0baf3d124
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-29 12:11:35 -0500

    RegisterPage 1

--- lib/ui/pages/scanner_page.dart
+++ lib/ui/pages/scanner_page.dart
@@ -1,9 +1,16 @@
 
+import 'package:codigo2_qr/ui/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 class ScannerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage()));
+        },
+      ),
+    );
   }
 }
