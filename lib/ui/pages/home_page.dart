0c7160566872ed1637b2ec34959ae1810c1d9adf
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-29 10:24:05 -0500

    Dividers 2

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:codigo2_qr/ui/widgets/general_widget.dart';
+import 'package:codigo2_qr/ui/widgets/item_list_widget.dart';
 import 'package:codigo2_qr/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,82 +8,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("QR Storage"),
-      ),
+      backgroundColor: Color(0xff1E1E1E),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
-              padding: const  EdgeInsets.symmetric(horizontal: 12, vertical: 14.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(14.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.06),
-                    blurRadius: 12,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Lorem ipsum dolor sit amet",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 15.0,
-                          ),
-                        ),
-                        divider3,
-                        Text(
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-                          style: TextStyle(
-                            height: 1.2,
-                            color: Colors.black54,
-                          ),
-                        ),
-                        divider6,
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              Assets.iconCalendar,
-                              height: 16.0,
-                              color: Colors.black54,
-                            ),
-                            Text(
-                              "12/12/2022",
-                              style: TextStyle(
-                                color: Colors.black54,
-                              ),
-                            ),
-                            dividerWidth6,
-                            SvgPicture.asset(
-                              Assets.iconTime,
-                              height: 16.0,
-                              color: Colors.black54,
-                            ),
-                            Text(
-                              "10:30 am",
-                              style: TextStyle(
-                                color: Colors.black54,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  SvgPicture.asset(Assets.iconLink),
-                ],
-              ),
-            ),
+            ItemListWidget(),
+            ItemListWidget(),
+            ItemListWidget(),
+            ItemListWidget(),
+            ItemListWidget(),
           ],
         ),
       ),
