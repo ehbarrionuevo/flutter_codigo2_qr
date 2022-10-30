13c68d60c18b2e5526287449b8bbe2ec04243b53
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-30 12:10:36 -0500

    QR Scanner 3.1

--- lib/ui/pages/scanner_page.dart
+++ lib/ui/pages/scanner_page.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:codigo2_qr/ui/general/colors.dart';
 import 'package:codigo2_qr/ui/pages/register_page.dart';
+import 'package:codigo2_qr/ui/widgets/button_normal_widget.dart';
+import 'package:codigo2_qr/ui/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
@@ -11,7 +13,6 @@ class ScannerPage extends StatefulWidget {
 }
 
 class _ScannerPageState extends State<ScannerPage> {
-
   Barcode? result;
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
@@ -25,11 +26,10 @@ class _ScannerPageState extends State<ScannerPage> {
     controller!.resumeCamera();
   }
 
-
   Widget _buildQrView(BuildContext context) {
     // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
-        MediaQuery.of(context).size.height < 400)
+            MediaQuery.of(context).size.height < 400)
         ? 250.0
         : 300.0;
     // To ensure the Scanner view is properly sizes after rotation
@@ -38,11 +38,11 @@ class _ScannerPageState extends State<ScannerPage> {
       key: qrKey,
       onQRViewCreated: _onQRViewCreated,
       overlay: QrScannerOverlayShape(
-          borderColor: kBrandPrimaryColor,
-          borderRadius: 14,
-          borderLength: 26,
-          borderWidth: 7,
-          cutOutSize: scanArea,
+        borderColor: kBrandPrimaryColor,
+        borderRadius: 14,
+        borderLength: 26,
+        borderWidth: 7,
+        cutOutSize: scanArea,
       ),
       onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
     );
@@ -55,9 +55,13 @@ class _ScannerPageState extends State<ScannerPage> {
     controller.scannedDataStream.listen((scanData) {
       setState(() {
         result = scanData;
-        print("wwwwwwwwwwwwwwwwwwww ${result}");
+        if (result != null) {
+          print("wwwwwwwwwwwwwwwwwwww ${result!.code}");
+        }
       });
     });
+    controller.pauseCamera();
+    controller.resumeCamera();
   }
 
   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
@@ -91,7 +95,27 @@ class _ScannerPageState extends State<ScannerPage> {
           ),
           Expanded(
             flex: 1,
-            child: SizedBox(),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0,),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: kBrandSecondaryColor,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Por favor, escanea un código QR.",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                  divider14,
+                  ButtonNormalWidget(text: "Registrar QR", onPressed: (){},),
+                ],
+              ),
+            ),
           ),
         ],
       ),
