741a2b277c68a5a8145a5d108c81fe3091f0799f
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-05 10:30:44 -0500

    Provider 1.7

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -7,16 +7,21 @@ import 'package:flutter/material.dart';
 class DBProvider extends ChangeNotifier{
 
   List<QRModel> qrList = [];
+  int res = 0;
   bool isLoading = false;
 
   Future<void> getDataProvider() async{
     isLoading = true;
     notifyListeners();
-
     qrList = await DBAdmin.db.getQRData();
-
     isLoading = false;
     notifyListeners();
   }
+
+  Future<void> addRegister(QRModel  model) async{
+    res = await DBAdmin.db.insertQR(model);
+    getDataProvider();
+  }
+
 
 }
